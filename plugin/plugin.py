0b29348d950ea5fd7f6fc024c7e0367608dfa23c
Ján Janech <janusko@users.noreply.github.com>
2016-01-15 10:33:50 +0100

    IPython terminal can be opened by clicking on a toolbar button

--- plugin/plugin.py
+++ plugin/plugin.py
@@ -1,7 +1,23 @@
+from functools import partial
+
 from org.umlfri.api.main_loops.qt import QtMainLoop
 
 
+class Terminal:
+    def __init__(self, app):
+        self.__app = app
+    
+    def open(self):
+        import inprocess_qtconsole
+        
+        self.__app.actions["open"].enabled = False
+        self.__app.actions["open"].triggered.disconnect(self.open)
+        inprocess_qtconsole.main(i=self.__app)
+
+
+def get_main_loop():
+    return QtMainLoop()
+
+
 def plugin_main(app):
-    import inprocess_qtconsole
-    app.set_main_loop(QtMainLoop())
-    inprocess_qtconsole.main(i=app)
+    app.actions["open"].triggered.connect(Terminal(app).open)
